07555051fa25fd8bc29355ba5e64e216000a63ff
Raphael Halmas Silva <raphaelhalmas@gmail.com>
2021-01-02 09:35:54 -0300

    alteracoes relacionadas a exclusao de arquivos

--- dist/config/autenticacaoProperties.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    tokenJWT: {
-        secretOrPrivateKey: 'bb757a278f392d128cf1157019d3d936',
-        expiresIn: '1d'
-    }
-};
